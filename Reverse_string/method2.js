cef2a9c0f4e729e40f013ddbcf00e8115a544460
Shivam Chaudhary <shivamchaudhary310899@gmail.com>
2023-02-19 17:23:59 +0530

    basic js

---
+++ Reverse_string/method2.js
@@ -0,0 +1,10 @@
+/*for decrementing */
+
+function reverseString(str){
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--){
+        newString += str[i];
+    }
+    return newString;
+}
+reverseString('hello');
