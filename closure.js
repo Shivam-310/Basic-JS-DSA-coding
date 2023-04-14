88df0422e064e993d3d827ae782497507565d279
Shivam Chaudhary <shivamchaudhary310899@gmail.com>
2023-04-14 23:50:32 +0530

    JS

---
+++ closure.js
@@ -0,0 +1,13 @@
+function outerFunction() {
+    const outerVar = "I am from the outer function";
+  
+    function innerFunction() {
+      console.log(outerVar);
+    }
+  
+    return innerFunction;
+  }
+  
+  const inner = outerFunction();
+  inner(); // Output: "I am from the outer function"
+  
