02718c703cc36544ab8bd18ae6dfcea53155e948
Shivam Chaudhary <shivamchaudhary310899@gmail.com>
2023-04-17 23:52:06 +0530

    JS

--- sumArrayNumbers.js
+++ sumArrayNumbers.js
@@ -1,7 +1,10 @@
-function sumNumbers(arr) {
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-      sum += arr[i];
-    }
-    return sum;
-  }
+// function sumNumbers(arr) {
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//       sum += arr[i];
+//     }
+//     return sum;
+//   }
+
+  let aditya = null;
+  console.log(aditya);
