1fa7f0ae7af08140c0abf23da5a9b68d985b8015
Shivam Chaudhary <shivamchaudhary310899@gmail.com>
2023-05-10 14:56:31 +0530

    reverse linked list

---
+++ reverseLinkedList.js
@@ -0,0 +1,14 @@
+function reverseLinkedList(head) {
+    let prev = null;
+    let current = head;
+    
+    while (current !== null) {
+      const next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+    }
+    
+    return prev;
+  }
+  
